c1ddf2d68b76a6b6bcd6a43129242785404f032b
WeiJinjzH <991775996@qq.com>
2020-03-10 16:05:11 +0800

    lazy异步导入组件

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,26 @@
-import React, { Component, createContext } from 'react';
+import React, { lazy, Suspense } from 'react';
 import './App.css';
 
-const BetteryContext = createContext()
-const OnlineContext = createContext()
+const About =  lazy(() => import(/* webpackChunkName: "about" */'./About.jsx'))
 
-class Leaf extends Component {
-  static contextType = BetteryContext
-
-  render() {
-    const bettery = this.context
-    return (
-          <h1>bettery: {bettery}</h1>
-        )
-  }
-}
-
-class Middle extends Component {
-  render() {
-    return <Leaf />
-  }
-}
-
-class App extends Component {
+class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      bettery: 0,
-      online: false,
+      hasError: false,
     }
   }
+  componentDidCatch() {
+    this.setState({ hasError: true })
+  }
   render() {
+    if (this.state.hasError) {
+      return <div>error</div>
+    }
     return (
-      <BetteryContext.Provider value={this.state.bettery}>
-      <OnlineContext.Provider value={this.state.online}>
-        <button onClick={() => this.setState({ bettery: this.state.bettery + 1 })} type="button">Press</button>
-        <button onClick={() => this.setState({ online: !this.state.online })} type="button">Switch</button>
-        <Middle />
-      </OnlineContext.Provider>
-      </BetteryContext.Provider>
+      <Suspense fallback={<div>loading</div>}>
+        <About />
+      </Suspense>
     )
   }
 }
